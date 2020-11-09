0a591d61fcc2438f493e2b243fd53018cf8abf79
VittorioSanchez <vittorio.sanchez.pro@gmail.com>
2020-11-09 19:47:56 +0100

    Implements ROS, working stop button

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # The GEI-car's Human Machine Interface
+
 HMI for the GEI-car project
+
+Needs rosbridge_server installed.
+Needs apache2 server installed.
+
+
+To launch rosbridge_server:
+
+$ roslaunch rosbridge_server rosbridge_websocket.launch
+
+
+
--- components/development/commands.js
+++ components/development/commands.js
@@ -1,26 +1,24 @@
 function StopVehicle(){
-		  // Publishing a Topic
-	  // ------------------
+    // ------------------
 
-	  var cmdVel = new ROSLIB.Topic({
-	    ros : ros,
-	    name : '/cmd_vel',
-	    messageType : 'geometry_msgs/Twist'
-	  });
+    var cmdVel = new ROSLIB.Topic({
+    ros : ros,
+    name : '/cmd_vel',
+    messageType : 'geometry_msgs/Twist'
+    });
 
-	  var twist = new ROSLIB.Message({
-	    linear : {
-	      x : 0.0,
-	      y : 0.0,
-	      z : 0.0
-	    },
-	    angular : {
-	      x : 0.0,
-	      y : 0.0,
-	      z : 0.0
-	    }
-	  });
-	  cmdVel.publish(twist);
-
+    var twist = new ROSLIB.Message({
+    linear : {
+      x : 0.0,
+      y : 0.0,
+      z : 0.0
+    },
+    angular : {
+      x : 0.0,
+      y : 0.0,
+      z : 0.0
+    }
+    });
+    cmdVel.publish(twist);
     console.log("Stop the vehicle");
 }
