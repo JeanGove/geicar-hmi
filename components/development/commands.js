fb1110126a6740ea953b66b5b42e4777b15c3d00
VittorioSanchez <vittorio.sanchez.pro@gmail.com>
2020-11-09 13:29:54 +0100

    Add commandes ROS arrêt + connexion ROS

--- components/development/commands.js
+++ components/development/commands.js
@@ -1,3 +1,26 @@
 function StopVehicle(){
+		  // Publishing a Topic
+	  // ------------------
+
+	  var cmdVel = new ROSLIB.Topic({
+	    ros : ros,
+	    name : '/cmd_vel',
+	    messageType : 'geometry_msgs/Twist'
+	  });
+
+	  var twist = new ROSLIB.Message({
+	    linear : {
+	      x : 0.0,
+	      y : 0.0,
+	      z : 0.0
+	    },
+	    angular : {
+	      x : 0.0,
+	      y : 0.0,
+	      z : 0.0
+	    }
+	  });
+	  cmdVel.publish(twist);
+
     console.log("Stop the vehicle");
 }
